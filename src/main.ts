963e62a9fbfb02da65e94bfaf76e3ea0f969acdd
dev2820 <second28200@gmail.com>
2022-09-28 09:37:09 +0900

    feature/ pdf.js 사용을 위해 워커 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import * as pdfjsLib from 'pdfjs-dist';
 
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
 createApp(App).use(store).use(router).mount('#app');
