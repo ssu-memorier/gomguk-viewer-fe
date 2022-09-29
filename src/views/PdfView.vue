6fefd09688636e48b9ad52d6d13fac4ab89c96c0
dev2820 <second28200@gmail.com>
2022-09-29 12:58:47 +0900

    feature/ 파일 변경에 따라 페이지를 새로 생성하도록 코드 추가

--- src/views/PdfView.vue
+++ src/views/PdfView.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="pdf-view">
         <PdfPage
-            v-for="pageNum in pdfPageNumList"
-            :key="pageNum"
-            :pageNum="pageNum"
+            v-for="PageIndex in pageIndexList"
+            :key="PageIndex.key"
+            :pageIndex="PageIndex.idx"
         >
         </PdfPage>
     </div>
@@ -14,24 +14,38 @@ import PdfPage from '@/components/PdfPage.vue';
 import { PdfState } from '@/Interface/PdfState';
 import { usePdfStore } from '@/store/pdf';
 import { onBeforeMount, ref } from 'vue';
-
+type PageIndex = {
+    idx: number;
+    key: string;
+};
 /**
  * tempPdfUrl은 파일을 불러오는 기능을 추가하기 전 임시로 PDF를 불러오기 위해 사용합니다.
  * 다음 브랜치에서 삭제할 예정입니다.
  */
 const tempPdfUrl = '/compressed.tracemonkey-pldi-09.pdf';
 const pdfStore = usePdfStore();
-const pdfPageNumList = ref<number[]>([]);
+const pageIndexList = ref<PageIndex[]>([]);
 onBeforeMount(() => {
     pdfStore.setPdfFromUrl(tempPdfUrl);
 });
 
 pdfStore.$subscribe('doc', (state: PdfState) => {
-    if (state.doc) {
-        const maxPageNum = pdfStore.doc.numPages;
-        for (let i = 1; i <= maxPageNum; i++) {
-            pdfPageNumList.value.push(i);
-        }
-    }
+    if (!state.doc) return;
+
+    pageIndexList.value = createPageIndexList(
+        state.fileName,
+        state.doc.numPages
+    );
 });
+
+function createPageIndexList(fileName: string, maxPageNum: number) {
+    const list = [];
+    for (let i = 1; i <= maxPageNum; i++) {
+        list.push({
+            idx: i,
+            key: `${fileName}-${i}`,
+        });
+    }
+    return list;
+}
 </script>
