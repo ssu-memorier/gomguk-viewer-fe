da7d274cd7fa291b6fe1f33d4c67f0d0e321462e
dev2820 <second28200@gmail.com>
2022-09-29 12:35:37 +0900

    feature/ PdfLoadButton 스타일 수정 및 부분 상수화

--- src/components/button/PdfLoadButton.vue
+++ src/components/button/PdfLoadButton.vue
@@ -1,16 +1,20 @@
 <template>
-    <label
-        >PDF 선택
+    <label>
+        PDF 선택
         <input
             type="file"
             id="fileLoad"
             @change="loadFileHandler"
-            accept=".pdf"
+            :accept="PDF.EXTENSION"
         />
     </label>
 </template>
 
 <script setup lang="ts">
+/**
+ * PdfLoadButton은 PDF 파일을 불러올 수 있는 버튼입니다.
+ * PDF 파일만을 불러오도록하며, 불러온 파일이 PDF인지 검사합니다.
+ */
 import { defineEmits } from 'vue';
 import PDF from '@/constants/PDF';
 
@@ -25,15 +29,27 @@ function loadFileHandler(e: Event) {
     const fileList = $input.files;
 
     if (!fileList || fileList.length <= 0) return;
-
     if (!isPdfFile(fileList[0])) return;
-    console.log(fileList[0]);
+
     emit('load', fileList[0]);
 }
 
-function isPdfFile(file: File) {
+function isPdfFile(file: File): boolean {
     return file.type === PDF.TYPE;
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+label {
+    display: inline-block;
+    cursor: pointer;
+    border: 1px solid black;
+    border-radius: 4px;
+    margin: auto 0;
+    padding: 0.25rem;
+}
+input[type='file'] {
+    width: 0;
+    height: 0;
+}
+</style>
