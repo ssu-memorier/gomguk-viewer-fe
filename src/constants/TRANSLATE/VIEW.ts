dd3f71c35b5a877da3bddd2d5697849061d38186
dev2820 <second28200@gmail.com>
2022-09-30 20:59:54 +0900

    feature/ 번역기 위치 조절 기능 추가

--- src/constants/TRANSLATE/VIEW.ts
+++ src/constants/TRANSLATE/VIEW.ts
@@ -3,4 +3,6 @@ export default {
     NAME: '번역기',
     CLOSE: '닫기',
     OPEN: '열기',
+    LEFT_TOP: '왼쪽 위로 이동',
+    RIGHT_TOP: '오른쪽 위로 이동',
 };
--- src/views/TranslatorView.vue
+++ src/views/TranslatorView.vue
@@ -1,10 +1,27 @@
 <template>
-    <div class="translatorView">
+    <div
+        class="translatorView card"
+        :class="{
+            leftTop: position === POSITION.LEFT_TOP,
+            rightTop: position === POSITION.RIGHT_TOP,
+        }"
+    >
         <div class="translatorHeader">
             <span class="name">{{ TRANSLATE.VIEW.NAME }}</span>
-            <button class="minimize" @click="minimizeHandler">
-                {{ isMinimized ? TRANSLATE.VIEW.OPEN : TRANSLATE.VIEW.CLOSE }}
-            </button>
+            <span>
+                <button class="position" @click="positionHandler">
+                    {{
+                        position === POSITION.LEFT_TOP
+                            ? TRANSLATE.VIEW.RIGHT_TOP
+                            : TRANSLATE.VIEW.LEFT_TOP
+                    }}
+                </button>
+                <button class="minimize" @click="minimizeHandler">
+                    {{
+                        isMinimized ? TRANSLATE.VIEW.OPEN : TRANSLATE.VIEW.CLOSE
+                    }}
+                </button>
+            </span>
         </div>
         <language-translator
             class="translator"
@@ -19,16 +36,28 @@
  */
 import { ref } from 'vue';
 import TRANSLATE from '@/constants/TRANSLATE';
+import POSITION from '@/constants/POSITION';
+import type { PositionType } from '@/types/PositionType';
 import LanguageTranslator from '@/components/LanguageTranslator.vue';
 
 const isMinimized = ref<boolean>(false);
+const position = ref<PositionType>(POSITION.RIGHT_TOP);
+
 function minimizeHandler() {
     isMinimized.value = !isMinimized.value;
+}
+function positionHandler() {
+    if (position.value === POSITION.LEFT_TOP) {
+        position.value = POSITION.RIGHT_TOP;
+    } else {
+        position.value = POSITION.LEFT_TOP;
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 div.translatorView {
+    position: fixed;
     border-radius: var(--border-radius);
     width: 400px;
     height: fit-content;
@@ -46,5 +75,13 @@ div.translatorView {
     .translator {
         height: 600px;
     }
+}
+div.translatorView.leftTop {
+    top: calc(var(--header-height) + 20px);
+    left: 20px;
+}
+div.translatorView.rightTop {
+    top: calc(var(--header-height) + 20px);
+    right: 20px;
 }
 </style>
