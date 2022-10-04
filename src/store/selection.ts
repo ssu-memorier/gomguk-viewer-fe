91b7e7d434ca2949bb62e1e7efdef449454e900d
dev2820 <second28200@gmail.com>
2022-10-05 01:38:39 +0900

    feature/ selectedText 외에 getSelectedText 함수 추가

--- src/store/selection.ts
+++ src/store/selection.ts
@@ -1,24 +1,26 @@
 import { defineStore } from 'pinia';
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import getSerializedText from '@/utils/getSerializedText';
 
 export const useSelectionStore = defineStore('selection', () => {
     const selection = ref<Selection>();
     const selectedText = ref<string>('');
 
-    watch(selection, (newSelection) => {
-        if (newSelection) {
-            selectedText.value = getSerializedText(newSelection.toString());
-        }
-    });
-
     function setSelection(newSelection: Selection) {
         selection.value = newSelection;
+        selectedText.value = getSerializedText(newSelection.toString());
     }
 
+    function getSelectedText() {
+        if (selection.value) {
+            return getSerializedText(selection.value.toString());
+        }
+        return '';
+    }
     return {
         selection,
         selectedText,
         setSelection,
+        getSelectedText,
     };
 });
