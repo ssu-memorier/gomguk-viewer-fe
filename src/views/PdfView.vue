737d62696beaf35df7c0e9956c5734de5da77008
dev2820 <second28200@gmail.com>
2022-10-04 18:31:50 +0900

    feature/ selection popup 화면에 추가

--- src/views/PdfView.vue
+++ src/views/PdfView.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="pdfView" v-if="isPdfExist" @copy="copyHandler">
-        <PdfPage
-            v-for="PageIndex in pageIndexList"
-            :key="PageIndex.key"
-            :pageIndex="PageIndex.idx"
-        >
-        </PdfPage>
-    </div>
-    <div class="noPdf" v-else>
-        <p>파일을 불러와주세요</p>
+    <div ref="$pdfView">
+        <SelectionPopup
+            class="selectionPopup"
+            ref="$selectionPopup"
+            :isShow="isPopupShow"
+        ></SelectionPopup>
+        <div class="pageContainer" v-if="isPdfExist" @copy="copyHandler">
+            <PdfPage
+                v-for="PageIndex in pageIndexList"
+                :key="PageIndex.key"
+                :pageIndex="PageIndex.idx"
+            >
+            </PdfPage>
+        </div>
+        <div class="noPdf" v-else>
+            <p>파일을 불러와주세요</p>
+        </div>
     </div>
 </template>
 
@@ -16,9 +23,11 @@
 import PdfPage from '@/components/PdfPage.vue';
 import { PdfState } from '@/Interface/PdfState';
 import { usePdfStore } from '@/store/pdf';
-import { ref } from 'vue';
-import getCopiedText from '@/utils/getCopiedText';
+import { ref, onMounted } from 'vue';
+import getCopiedText from '@/utils/getSelectionText';
 import CLIPBOARD from '@/constants/CLIPBOARD';
+import SELECTION from '@/constants/SELECTION';
+import SelectionPopup from '@/components/popup/SelectionPopup.vue';
 
 type PageIndex = {
     idx: number;
@@ -28,6 +37,19 @@ type PageIndex = {
 const pdfStore = usePdfStore();
 const pageIndexList = ref<PageIndex[]>([]);
 const isPdfExist = ref<boolean>(false);
+const $selectionPopup = ref();
+const $pdfView = ref();
+const isPopupShow = ref<boolean>(false);
+
+onMounted(() => {
+    $pdfView.value.addEventListener(
+        SELECTION.SELECTION_END_EVENT,
+        selectionEndHandler
+    );
+    $pdfView.value.addEventListener('mousedown', () => {
+        isPopupShow.value = false;
+    });
+});
 
 pdfStore.$subscribe('doc', (state: PdfState) => {
     if (!state.doc) {
@@ -63,16 +85,34 @@ function copyHandler(evt: ClipboardEvent) {
     clipboard.setData(CLIPBOARD.CONTENT_TYPE, copiedText);
     evt.preventDefault();
 }
+
+function selectionEndHandler(evt: CustomEvent) {
+    isPopupShow.value = true;
+    const { clientX, clientY } = evt.detail;
+    const { left, top } = $pdfView.value.getBoundingClientRect();
+
+    const x = Math.max(clientX - left);
+    const y = Math.max(clientY - top);
+    $selectionPopup.value.$el.style.left = `${x}px`;
+    $selectionPopup.value.$el.style.top = `${y}px`;
+}
 </script>
 
 <style lang="scss" scoped>
-.pdfView {
+.selectionPopup {
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 200;
+}
+.pageContainer {
     width: 100%;
     height: 100%;
     padding-top: 1rem;
     padding-bottom: 10rem;
     display: flex;
     flex-direction: column;
+    z-index: 1;
 }
 .noPdf {
     position: relative;
@@ -84,6 +124,7 @@ function copyHandler(evt: ClipboardEvent) {
         margin-top: -100px;
         left: 50%;
         margin-left: -100px;
+        user-select: none;
     }
 }
 </style>
