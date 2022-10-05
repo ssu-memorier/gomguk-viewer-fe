33d133a1f5dfff04a08a38fb3a290cb3fb058051
dev2820 <second28200@gmail.com>
2022-10-05 23:55:20 +0900

    feature/ 언어 타입 생성

--- src/constants/TRANSLATOR/index.ts
+++ src/constants/TRANSLATOR/index.ts
@@ -1,7 +1,9 @@
 import VIEW from '@/constants/TRANSLATOR/VIEW';
+import LANGUAGES from '@/constants/TRANSLATOR/LANGUAGES';
 
 export default {
     VIEW,
+    LANGUAGES,
     DOMAIN: process.env.VUE_APP_TRANSLATE_DOMAIN,
     TRANSLATE_URL: '/translate',
     LATENCY: 200,
