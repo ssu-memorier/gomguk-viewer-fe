01f38b8e028940a4a2b191acfcf07d99f025f80a
dev2820 <second28200@gmail.com>
2022-10-06 00:28:27 +0900

    chore/ 주석 수정

--- src/Interface/api/IRequestTranslatedTextParams.ts
+++ src/Interface/api/IRequestTranslatedTextParams.ts
@@ -1,6 +1,6 @@
 import type { LanguageType } from '@/types/LanguageType';
 /**
- * requestTranslatedTextParams 요청시 사용되는 파라미터입니다.
+ * 텍스트 번역 API에 사용되는 파라미터 인터페이스 입니다.
  */
 export interface IRequestTranslatedTextParams {
     source: LanguageType;
