0e16b8443b9fc2158788fb11188bcf697416d1f0
dev2820 <second28200@gmail.com>
2022-10-12 14:19:53 +0900

    refactor/ getLine 주석 추가

--- src/utils/line/getLineMap.ts
+++ src/utils/line/getLineMap.ts
@@ -1,6 +1,12 @@
 import TOKEN from '@/constants/TOKEN';
 import Line from '@/classes/Line';
 
+/**
+ * 몇번 줄이 selection에 포함되는지 저장하는 Map을 반환합니다.
+ * Map<줄 번호, Line 객체>
+ * @param tokens selection에 포함된 토큰 리스트
+ * @returns 각 줄에 해당하는 Line이 포함된 Map
+ */
 export default function getLineMap(tokens: HTMLElement[]) {
     return tokens.reduce((map, $token) => {
         const tokenInfo = $token.dataset;
