b5f9f8cbe32851424b5f35dd3619d09187bbe9d7
dev2820 <second28200@gmail.com>
2022-10-12 13:17:51 +0900

    refactor/ selectionStore 구조 단순화

--- src/store/selection.ts
+++ src/store/selection.ts
@@ -1,23 +1,20 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import getSerializedTextFromNodes from '@/utils/getSerializedTextFromNodes';
 import getSelectedPageIndex from '@/utils/getSelectedPageIndex';
 
 export const useSelectionStore = defineStore('selection', () => {
     const range = ref<Range | null>();
-    const isSelectionExist = ref<boolean>();
-    const selectedPageIndex = ref<number | null>(null);
-
+    const isSelectionExist = computed(() => {
+        return !!range.value && !range.value.collapsed;
+    });
+    const selectedPageIndex = computed(() => {
+        return isSelectionExist.value
+            ? getSelectedPageIndex(range.value as Range)
+            : null;
+    });
     function setSelection(newSelection: Selection | null) {
         range.value = newSelection?.getRangeAt(0);
-
-        if (range.value && !range.value.collapsed) {
-            isSelectionExist.value = true;
-            selectedPageIndex.value = getSelectedPageIndex(range.value);
-        } else {
-            isSelectionExist.value = false;
-            selectedPageIndex.value = null;
-        }
     }
 
     function getSelectedText() {
