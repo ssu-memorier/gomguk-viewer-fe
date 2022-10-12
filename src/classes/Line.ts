d4bb43531c2fa6a8e2538932a53d2930def694c7
dev2820 <second28200@gmail.com>
2022-10-12 14:30:13 +0900

    refactor/ Line 주석 추가, 구조 단순화

--- src/classes/Line.ts
+++ src/classes/Line.ts
@@ -1,11 +1,21 @@
 import TOKEN from '@/constants/TOKEN';
 import { IPos } from '@/Interface/IPos';
+
+/**
+ * Line 클래스는 selection된 줄 정보를 저장합니다.
+ * selection 영역의 왼쪽 위 좌표와 오른쪽 아래 좌표를 저장하며 이 두 점의 정보로
+ * 사각형 영역 정보를 나타냅니다.
+ */
 export default class Line {
     lineNum;
-    right = 0;
-    bottom = 0;
-    left = Infinity;
-    top = Infinity;
+    startPoint: IPos = {
+        x: Infinity,
+        y: Infinity,
+    };
+    endPoint: IPos = {
+        x: 0,
+        y: 0,
+    };
     #tokens = [] as HTMLElement[];
     constructor(lineNum: number) {
         this.lineNum = lineNum;
@@ -22,23 +32,25 @@ export default class Line {
             $token.dataset[TOKEN.DATASET.BOTTOM] as string
         );
 
-        this.left = Math.min(this.left, tokenLeft);
-        this.top = Math.min(this.top, tokenTop);
-        this.right = Math.max(this.right, tokenRight);
-        this.bottom = Math.max(this.left, tokenBottom);
+        this.startPoint = {
+            x: Math.min(this.startPoint.x, tokenLeft),
+            y: Math.min(this.startPoint.y, tokenTop),
+        };
+        this.endPoint = {
+            x: Math.max(this.endPoint.x, tokenRight),
+            y: Math.max(this.endPoint.y, tokenBottom),
+        };
     }
     setStartPos(pos: IPos) {
-        this.left = pos.x;
-        this.top = pos.y;
+        this.startPoint = pos;
     }
     setEndPos(pos: IPos) {
-        this.right = pos.x;
-        this.bottom = pos.y;
+        this.endPoint = pos;
     }
     get lineHeight() {
-        return Math.max(this.bottom - this.top, 0);
+        return Math.max(this.endPoint.y - this.startPoint.y, 0);
     }
     get lineWidth() {
-        return Math.max(this.right - this.left, 0);
+        return Math.max(this.endPoint.x - this.startPoint.x, 0);
     }
 }
