85e457472d1672946554cb54f8c9718e0e698a82
dev2820 <second28200@gmail.com>
2022-10-14 13:08:53 +0900

    feature/ POPUP에 highlight 추가 & POPUP관련 상수를 POPUP폴더로 모음

---
+++ src/constants/POPUP/MENUS.ts
@@ -0,0 +1,14 @@
+export const TRANSLATE = {
+    NAME: '번역하기',
+    TYPE: 'translate',
+};
+
+export const COPY = {
+    NAME: '복사',
+    TYPE: 'copy',
+};
+
+export const HIGHLIGHT = {
+    NAME: '강조',
+    TYPE: 'highlight',
+};
---
+++ src/constants/POPUP/index.ts
@@ -0,0 +1,5 @@
+import * as MENUS from '@/constants/POPUP/MENUS';
+import VIEW from '@/constants/POPUP/VIEW';
+import DATASET from '@/constants/POPUP/DATASET';
+
+export default { MENUS, VIEW, DATASET };
