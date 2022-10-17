f43518de7b7227348e05078486071831645db7ec
dev2820 <second28200@gmail.com>
2022-10-17 23:36:17 +0900

    feature/ 번역기 이동 기능 제거

--- src/views/TranslatorView.vue
+++ src/views/TranslatorView.vue
@@ -1,51 +1,29 @@
 <template>
-    <div
-        class="translatorView card"
-        :class="{
-            leftTop: position === POSITION.LEFT_TOP,
-            rightTop: position === POSITION.RIGHT_TOP,
-        }"
-    >
+    <div class="translatorView card">
         <div class="translatorHeader">
-            <span class="name">{{ TRANSLATOR.VIEW.NAME }}</span>
-            <span>
-                <button class="position" @click="positionHandler">
-                    {{
-                        position === POSITION.LEFT_TOP
-                            ? TRANSLATOR.VIEW.RIGHT_TOP
-                            : TRANSLATOR.VIEW.LEFT_TOP
-                    }}
-                </button>
-                <button class="minimize" @click="minimizeHandler">
-                    {{
-                        isMinimized
-                            ? TRANSLATOR.VIEW.OPEN
-                            : TRANSLATOR.VIEW.CLOSE
-                    }}
-                </button>
-            </span>
-        </div>
-        <div class="translatorLanguageSelect">
-            <select @change="sourceLanguageHandler">
-                <option
-                    v-for="LANG in LANGUAGES"
-                    :key="LANG.KEY"
-                    :value="LANG.KEY"
-                    :selected="LANG.KEY === translatorStore.source"
-                >
-                    {{ LANG.NAME }}
-                </option>
-            </select>
-            <select @change="targetLanguageHandler">
-                <option
-                    v-for="LANG in LANGUAGES"
-                    :key="LANG.KEY"
-                    :value="LANG.KEY"
-                    :selected="LANG.KEY === translatorStore.target"
-                >
-                    {{ LANG.NAME }}
-                </option>
-            </select>
+            <div class="name">{{ TRANSLATOR.VIEW.NAME }}</div>
+            <div class="translatorLanguageSelect">
+                <select @change="sourceLanguageHandler">
+                    <option
+                        v-for="LANG in LANGUAGES"
+                        :key="LANG.KEY"
+                        :value="LANG.KEY"
+                        :selected="LANG.KEY === translatorStore.source"
+                    >
+                        {{ LANG.NAME }}
+                    </option>
+                </select>
+                <select @change="targetLanguageHandler">
+                    <option
+                        v-for="LANG in LANGUAGES"
+                        :key="LANG.KEY"
+                        :value="LANG.KEY"
+                        :selected="LANG.KEY === translatorStore.target"
+                    >
+                        {{ LANG.NAME }}
+                    </option>
+                </select>
+            </div>
         </div>
         <language-translator
             class="translator"
@@ -60,36 +38,13 @@
  */
 import { ref } from 'vue';
 import TRANSLATOR from '@/constants/TRANSLATOR';
-import POSITION from '@/constants/POSITION';
 import LANGUAGES from '@/constants/TRANSLATOR/LANGUAGES';
-import type { PositionType } from '@/types/PositionType';
 import LanguageTranslator from '@/components/LanguageTranslator.vue';
 import { useTranslatorStore } from '@/store/translator';
 import { LanguageType } from '@/types/LanguageType';
 
 const translatorStore = useTranslatorStore();
-const position = ref<PositionType>(POSITION.RIGHT_TOP);
 const isMinimized = ref<boolean>(false);
-
-translatorStore.$subscribe(() => {
-    setMinimize(false);
-});
-
-function setMinimize(isMinimize: boolean) {
-    isMinimized.value = isMinimize;
-}
-
-function minimizeHandler() {
-    setMinimize(!isMinimized.value);
-}
-
-function positionHandler() {
-    if (position.value === POSITION.LEFT_TOP) {
-        position.value = POSITION.RIGHT_TOP;
-    } else {
-        position.value = POSITION.LEFT_TOP;
-    }
-}
 
 function sourceLanguageHandler(evt: Event) {
     const $target = evt.target as HTMLOptionElement;
@@ -107,9 +62,6 @@ div.translatorView {
     overflow: hidden;
     background-color: #ccc;
     div.translatorHeader {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
         padding: 0.5rem;
         button {
             cursor: pointer;
@@ -118,13 +70,5 @@ div.translatorView {
     .translator {
         height: 600px;
     }
-}
-div.translatorView.leftTop {
-    top: calc(var(--header-height) + 20px);
-    left: 20px;
-}
-div.translatorView.rightTop {
-    top: calc(var(--header-height) + 20px);
-    right: 20px;
 }
 </style>
