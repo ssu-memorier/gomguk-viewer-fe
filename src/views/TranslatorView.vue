84f3cf9d664fc5a67622cc48fdf20c7a338b2062
dev2820 <second28200@gmail.com>
2022-10-18 10:44:55 +0900

    refactor/ 안쓰는 상수 참조 제거, 번역기 header 크기 제한 제거

--- src/views/TranslatorView.vue
+++ src/views/TranslatorView.vue
@@ -36,7 +36,6 @@
  * TrasnlatorView는 텍스트를 입력하면 번역된 결과를 보여주는 뷰입니다.
  */
 import { ref } from 'vue';
-import TRANSLATOR from '@/constants/TRANSLATOR';
 import LANGUAGES from '@/constants/TRANSLATOR/LANGUAGES';
 import LanguageTranslator from '@/components/LanguageTranslator.vue';
 import { useTranslatorStore } from '@/store/translator';
@@ -62,7 +61,6 @@ div.translatorView {
     display: flex;
     flex-direction: column;
     div.translatorHeader {
-        height: 30px;
         flex-shrink: 0;
         padding: 0.5rem;
         button {
