ad8446be0e45fab6bfe43e039f098eafe152797c
dev2820 <second28200@gmail.com>
2022-10-19 15:27:46 +0900

    refactor/ copyCanvas가 canvas 요소를 받아 복사하도록 수정

--- src/utils/copyCanvas.ts
+++ src/utils/copyCanvas.ts
@@ -1,4 +1,13 @@
-export default function copyCanvas(ctx: CanvasRenderingContext2D) {
+/**
+ * 캔버스 요소를 복사해 반환합니다.
+ * @param canvas 복사할 canvas 요소
+ * @returns {HTMLCanvasElement | undefined}
+ */
+export default function copyCanvas(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) return;
+
     const { width, height } = ctx.canvas;
     const originImg = ctx.getImageData(0, 0, width, height);
     const copiedCanvas = document.createElement('canvas');
