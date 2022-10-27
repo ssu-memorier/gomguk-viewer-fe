221865c2148a2ef3389acba12776314c31070ac4
dev2820 <second28200@gmail.com>
2022-10-27 11:37:05 +0900

    feature/ other-means 컴포넌트 생성, 다른 뜻이 있으면 출력하도록 로직 수정

---
+++ src/components/translator/LanguageTranslator.vue
@@ -0,0 +1,88 @@
+<template>
+    <button @click="toggleShowOriginText">
+        {{
+            isShowOriginText
+                ? TRANSLATOR.VIEW.SHOW_TRANSLATED
+                : TRANSLATOR.VIEW.SHOW_ORIGIN
+        }}
+    </button>
+    <div class="translator">
+        <div class="translatorTextarea">
+            <div class="translated" disabled>
+                {{
+                    isShowOriginText
+                        ? translatorStore.originalText
+                        : translatorStore.translatedText
+                }}
+            </div>
+        </div>
+        <hr v-show="otherMeansExist" />
+        <div class="translatorOtherMeans" v-show="otherMeansExist">
+            <other-means
+                v-for="(kind, idx) in allKinds"
+                :key="kind"
+                :kind="kind"
+                :means="allMeans[idx]"
+            ></other-means>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+/**
+ * LanguageTranslator는 텍스트를 입력하면 번역된 결과를 보여주는 컴포넌트 입니다.
+ */
+import { ref, computed } from 'vue';
+import { useTranslatorStore } from '@/store/translator';
+import TRANSLATOR from '@/constants/TRANSLATOR';
+import OtherMeans from './OtherMeans.vue';
+const translatorStore = useTranslatorStore();
+const isShowOriginText = ref<boolean>(false);
+const otherMeansExist = computed(() => !!translatorStore.allTranslations);
+const allKinds = computed(() => {
+    const allTrans = translatorStore.allTranslations;
+    return allTrans ? Object.keys(allTrans) : [];
+});
+const allMeans = computed(() => {
+    const allTrans = translatorStore.allTranslations;
+    return allTrans ? Object.values(allTrans) : [];
+});
+
+function toggleShowOriginText() {
+    isShowOriginText.value = !isShowOriginText.value;
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/constants/TRANSLATOR';
+div.translator {
+    background-color: $TRANSLATOR-BG-COLOR;
+    overflow: auto;
+    height: 100%;
+}
+div.translatorTextarea {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    textarea.origin,
+    div.translated {
+        color: $TRANSLATOR-COLOR;
+        font-size: $TRANSLATOR-FONT-SIZE;
+        border: none;
+        padding: $TRANSLATOR-PADDING;
+        box-sizing: border-box;
+        text-align: left;
+        margin: 0;
+        flex-grow: 1;
+    }
+    textarea.origin {
+        resize: none;
+    }
+    textarea:focus {
+        outline: none;
+    }
+    div.translated {
+        overflow: scroll;
+    }
+}
+</style>
