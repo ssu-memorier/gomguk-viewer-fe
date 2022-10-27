ef2481e49ec335ac2f556b73e873186d96f26eee
dev2820 <second28200@gmail.com>
2022-10-27 11:48:41 +0900

    fix/ 절대경로가 잘못 반영되어 수정

--- src/components/translator/LanguageTranslator.vue
+++ src/components/translator/LanguageTranslator.vue
@@ -33,7 +33,7 @@
 import { ref, computed } from 'vue';
 import { useTranslatorStore } from '@/store/translator';
 import TRANSLATOR from '@/constants/TRANSLATOR';
-import OtherMeans from '@/components/OtherMeans.vue';
+import OtherMeans from '@/components/translator/OtherMeans.vue';
 const translatorStore = useTranslatorStore();
 const isShowOriginText = ref<boolean>(false);
 const otherMeansExist = computed(() => !!translatorStore.allTranslations);
