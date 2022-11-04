b016ef94a7e65c38a97c0fe339139ba5ec8640d2
dev2820 <second28200@gmail.com>
2022-11-04 18:29:13 +0900

    feature/ 파일 불러오기 API 함수 생성

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -2,6 +2,7 @@ import STORAGE from '@/constants/STORAGE';
 import createResponse from '@/utils/createResponse';
 import getStorageModel from '@/utils/getStorageModel';
 import { IRequestFileListParams } from '@/Interface/api/IRequestFileListParams';
+import { IRequestFileParams } from '@/Interface/api/IRequestFileParams';
 import { Response } from '@/Interface/Response';
 
 const model = getStorageModel();
@@ -12,6 +13,20 @@ export async function requestFileList(
     try {
         const { id } = params;
         const response = await model.get(STORAGE.URL.LIST + id);
+        const result = response.data.contents;
+
+        return createResponse(true, result);
+    } catch (err) {
+        return createResponse(false);
+    }
+}
+
+export async function requestFile(
+    params: IRequestFileParams
+): Promise<Response> {
+    try {
+        const { id, key } = params;
+        const response = await model.get(`${STORAGE.URL.FILE}/${id}/${key}`);
         const result = response.data.contents;
 
         return createResponse(true, result);
