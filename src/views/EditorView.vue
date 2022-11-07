d76f41c4d72e3b67acb96388fc5b36b87be210ad
dev2820 <second28200@gmail.com>
2022-11-07 21:51:51 +0900

    feature/ view 요소로 감쌈

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="editorjs"></div>
+    <div class="editorView">
+        <div id="editorjs"></div>
+    </div>
 </template>
 <script setup lang="ts">
 import editor from '@/store/editor';
@@ -8,17 +10,21 @@ const editorJs = editor.createEditor('editorjs');
 </script>
 <style lang="scss" scoped>
 @import '@/assets/scss/theme';
-
-#editorjs {
-    background-color: $SURFACE-COLOR;
-    text-align: left;
-    max-width: calc(650px + 8rem);
+.editorView {
     width: 100%;
     height: 100%;
-    overflow: auto;
-    position: relative;
-    font-family: 'Nanum Gothic', 'Roboto', sans-serif;
-    transition: 0.3s;
-    padding: 1rem 5rem;
+    background-color: $SURFACE-COLOR;
+    padding: 1rem 1rem 1rem 5rem;
+    text-align: left;
+
+    #editorjs {
+        max-width: calc(650px + 8rem);
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        position: relative;
+        font-family: 'Nanum Gothic', 'Roboto', sans-serif;
+        transition: 0.3s;
+    }
 }
 </style>
