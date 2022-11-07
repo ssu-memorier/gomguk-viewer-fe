d47edd4e75a9727a484b63c6b66e2192df4b67b6
dev2820 <second28200@gmail.com>
2022-11-07 22:12:08 +0900

    feature/ tool 추가 (header,nested_list, toggle block, simple image)

--- src/constants/EDITOR/index.ts
+++ src/constants/EDITOR/index.ts
@@ -1,9 +1,7 @@
 import NestedList from '@editorjs/nested-list';
 import Header from '@editorjs/header';
 import ToggleBlock from 'editorjs-toggle-block';
-import LaTeX from '@/modules/editorjs-latex';
 import SimpleImage from '@editorjs/simple-image';
-import init from '@/assets/init.json';
 
 const HEADER_OPS = {
     class: Header,
@@ -24,11 +22,6 @@ const TOGGLE_BLOCK_OPS = {
     inlineToolbar: true,
 };
 
-const LATEX_OPS = {
-    class: LaTeX,
-    shortcut: 'CMD+SHIFT+M',
-};
-
 const SIMPLE_IMAGE_OPS = {
     class: SimpleImage,
 };
@@ -37,17 +30,13 @@ const TOOL_CONFIG = {
     header: HEADER_OPS,
     list: NESTED_LIST_OPS,
     toggle: TOGGLE_BLOCK_OPS,
-    latex: LATEX_OPS,
     image: SIMPLE_IMAGE_OPS,
 };
 
-const INIT_DATA = init;
 export default {
     HEADER_OPS,
     NESTED_LIST_OPS,
     TOGGLE_BLOCK_OPS,
     SIMPLE_IMAGE_OPS,
     TOOL_CONFIG,
-    LATEX_OPS,
-    INIT_DATA,
 };
