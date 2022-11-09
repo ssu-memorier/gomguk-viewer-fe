29bf22a078acd030f1620c287043de2b03ce0431
dev2820 <second28200@gmail.com>
2022-11-10 00:50:07 +0900

    refactor/ 하이라이트 관련 로직은 일단 주석

--- src/Interface/api/IRequestFileUpdateParams.ts
+++ src/Interface/api/IRequestFileUpdateParams.ts
@@ -1,11 +1,14 @@
 /**
  * 파일 업데이트 (에디터) API에 사용되는 파라미터 인터페이스 입니다.
+ *
+ * TODO:
+ * highlights 영역은 일단 빈 문자열을 보내고, 추후 하이라이트를 저장하는 브랜치에서 별도로 추가할 예정입니다
  */
 export interface IRequestFileUpdateParams {
     dir: string;
     key: string;
     data: {
         editor: object;
-        highlights: Array<any>;
+        highlights: '';
     };
 }
