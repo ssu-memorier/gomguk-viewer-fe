4d25a925ebbb449ff78475f3b663ab791f42f43a
dev2820 <second28200@gmail.com>
2022-11-09 19:46:22 +0900

    feature/ 파일리스트 불러오기 스토어 함수 생성

--- src/store/file/index.ts
+++ src/store/file/index.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import { useEditorStore } from '@/store/file/editor';
 import { useHighlightStore } from '@/store/file/highlight';
 import { usePdfStore } from '@/store/file/pdf';
-import { requestFileUpload } from '@/api/storage';
+import { requestFileUpload, requestFileList } from '@/api/storage';
 import MESSAGE from '@/constants/MESSAGE';
 import { ref } from 'vue';
 
@@ -11,8 +11,15 @@ const HighlightStore = useHighlightStore();
 const pdfStore = usePdfStore();
 
 export const useFileStore = defineStore('file', () => {
-    function fetchFileList() {
-        //TODO: 파일 리스트 불러오기 로직
+    async function fetchFileList() {
+        const response = await requestFileList({ id: 'test_id' });
+        if (!response.isSuccess) {
+            alert(MESSAGE.STORAGE.GET_LIST_FAILED);
+
+            return;
+        }
+
+        return response.data;
     }
 
     async function uploadFile(pdf: File) {
@@ -38,6 +45,7 @@ export const useFileStore = defineStore('file', () => {
     }
 
     return {
+        fetchFileList,
         uploadFile,
     };
 });
