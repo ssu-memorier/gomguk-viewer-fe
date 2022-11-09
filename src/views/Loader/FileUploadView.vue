999e6777ad47c4b51d2265c539ea2f2fcd51919c
dev2820 <second28200@gmail.com>
2022-11-10 01:18:36 +0900

    feature/ 파일 로드시 바로 불러오는 로직 삭제

--- src/views/Loader/FileUploadView.vue
+++ src/views/Loader/FileUploadView.vue
@@ -9,13 +9,11 @@ export default {
 <script setup lang="ts">
 import PdfLoadButton from '@/components/button/PdfLoadButton.vue';
 import { useFileStore } from '@/store/file';
-import { usePdfStore } from '@/store/file/pdf';
 import { ref } from 'vue';
 import MESSAGE from '@/constants/MESSAGE';
 
 const $loadButton = ref();
 const fileStore = useFileStore();
-const pdfStore = usePdfStore();
 
 async function loadPdfHandler(pdf: File) {
     const isSuccess = await fileStore.uploadFile(pdf);
@@ -24,17 +22,5 @@ async function loadPdfHandler(pdf: File) {
         alert(MESSAGE.STORAGE.UPLOAD_FAILED);
         return;
     }
-
-    setPdf(pdf);
-    notifyFileLoad();
-}
-
-function setPdf(pdf: File) {
-    pdfStore.setPdfFile(pdf);
-}
-
-function notifyFileLoad() {
-    const fileLoadEvent = new Event('loadfile', { bubbles: true });
-    $loadButton.value.$el.dispatchEvent(fileLoadEvent);
 }
 </script>
