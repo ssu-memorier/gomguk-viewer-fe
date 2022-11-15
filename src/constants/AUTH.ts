25877bbd31fb134361c58d25cdb63db4bd5446b1
dev2820 <second28200@gmail.com>
2022-11-15 11:43:51 +0900

    refactor/ AUTH 상수의 일부 내용을 const로 변경함 (타입 생성을 위함)

--- src/constants/AUTH.ts
+++ src/constants/AUTH.ts
@@ -1,7 +1,15 @@
+export const PROVIDER = {
+    KAKAO: 'kakao',
+    GOOGLE: 'google',
+} as const;
+
+export const URL = {
+    KAKAO_AUTH: '/auth/kakao',
+    GOOGLE_AUTH: '/auth/google',
+} as const;
+
 export default {
     DOMAIN: process.env.VUE_APP_AUTH_DOMAIN,
-    URL: {
-        KAKAO_AUTH: '/auth/kakao',
-        GOOGLE_AUTH: '/auth/google',
-    },
+    URL: URL,
+    PROVIDER: PROVIDER,
 };
