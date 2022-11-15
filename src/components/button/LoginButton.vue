ab8729f934d8b3c11c1ebe397a167bd4870d5340
dev2820 <second28200@gmail.com>
2022-11-15 12:19:58 +0900

    refactor/ 주석 제거

--- src/components/button/LoginButton.vue
+++ src/components/button/LoginButton.vue
@@ -12,10 +12,6 @@ import LoginView from '@/views/LoginView.vue';
 const isShow = ref<boolean>(false);
 
 function toggleAuthMethod() {
-    /**
-     * TODO: 카카오와 구글 중 선택할 수 있는 view를 보여준다.
-     */
-
     isShow.value = !isShow.value;
 }
 </script>
