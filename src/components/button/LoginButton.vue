5429ee44a230d7981597966276e4e978cfc19588
dev2820 <second28200@gmail.com>
2022-11-15 12:13:05 +0900

    feature/ 로그인 버튼 컴포넌트 생성

---
+++ src/components/button/LoginButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="showAuthMethod">
+        {{ props.label }}
+    </button>
+    <component :is="LoginView" v-show="isShow"></component>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+import LoginView from '@/views/LoginView.vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+});
+const isShow = ref<boolean>(false);
+
+function showAuthMethod() {
+    /**
+     * TODO: 카카오와 구글 중 선택할 수 있는 view를 보여준다.
+     */
+}
+</script>
+
+<style lang="scss" scoped></style>
