a2c71118500a142b960ba4dd22446f9d0700e76d
dev2820 <second28200@gmail.com>
2022-11-15 12:16:24 +0900

    feature/ 로그인 버튼이 slot을 사용하도록 수정

--- src/components/button/LoginButton.vue
+++ src/components/button/LoginButton.vue
@@ -1,26 +1,22 @@
 <template>
-    <button @click="showAuthMethod">
-        {{ props.label }}
+    <button @click="toggleAuthMethod">
+        <slot></slot>
     </button>
     <component :is="LoginView" v-show="isShow"></component>
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref } from 'vue';
+import { ref } from 'vue';
 import LoginView from '@/views/LoginView.vue';
 
-const props = defineProps({
-    label: {
-        type: String,
-        required: true,
-    },
-});
 const isShow = ref<boolean>(false);
 
-function showAuthMethod() {
+function toggleAuthMethod() {
     /**
      * TODO: 카카오와 구글 중 선택할 수 있는 view를 보여준다.
      */
+
+    isShow.value = !isShow.value;
 }
 </script>
 
