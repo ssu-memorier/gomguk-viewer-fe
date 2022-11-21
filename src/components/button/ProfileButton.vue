dbc23b398904860ccdd145c0c00bfd5ae9755112
dev2820 <second28200@gmail.com>
2022-11-21 22:39:21 +0900

    feature/ 프로필 아이콘을 나타내기 위한 프로필 버튼 컴포넌트 추가

---
+++ src/components/button/ProfileButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <round-button>
+        <img v-if="picture" :src="picture" />
+        <img v-else src="@/assets/images/svg/user.svg" />
+    </round-button>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import RoundButton from '@/components/button/RoundButton.vue';
+
+defineProps({
+    picture: {
+        type: String,
+        required: false,
+        validator(value: string) {
+            const urlReg =
+                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+
+            return urlReg.test(value);
+        },
+    },
+});
+</script>
