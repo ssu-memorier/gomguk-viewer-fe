52dde3730341a1b402f6ea738a5e4d3929213be1
dev2820 <second28200@gmail.com>
2022-11-21 22:07:04 +0900

    feature/ roundbutton에 title 속성 추가

--- src/components/button/RoundButton.vue
+++ src/components/button/RoundButton.vue
@@ -1,7 +1,16 @@
 <template>
-    <button>
+    <button :title="title">
         <slot></slot>
     </button>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+    title: {
+        type: String,
+        required: false,
+    },
+});
+</script>
 <style lang="scss" scoped></style>
