0c90a51e2eb20a9b1bbaf4c7935a17e9fd36ebf2
dev2820 <second28200@gmail.com>
2022-11-22 20:52:05 +0900

    feature/ guide view 중앙에 나타날 문자열, 버튼 추가

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -1,4 +1,13 @@
 <template>
-    <div>empty</div>
+    <div class="container">
+        <div class="center">
+            <span class="guideText"
+                >사용할 PDF를 불러오세요. 모르는 내용을 번역하고 중요한 내용을
+                노트에 필기해 논문을 집중해서 읽을 수 있습니다.</span
+            >
+            <button>파일 불러오기</button>
+        </div>
+    </div>
 </template>
 <script setup lang="ts"></script>
+<style scoped lang="scss"></style>
