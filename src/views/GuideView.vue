75f33f207a86dbbfeb1ad699a4d2ff5e85f24c4a
dev2820 <second28200@gmail.com>
2022-11-22 20:54:54 +0900

    style/ guideview에서 round버튼을 사용하도록 수정

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -5,9 +5,11 @@
                 >사용할 PDF를 불러오세요. 모르는 내용을 번역하고 중요한 내용을
                 노트에 필기해 논문을 집중해서 읽을 수 있습니다.</span
             >
-            <button>파일 불러오기</button>
+            <round-button class="load"> 파일 불러오기 </round-button>
         </div>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RoundButton from '@/components/button/RoundButton.vue';
+</script>
 <style scoped lang="scss"></style>
