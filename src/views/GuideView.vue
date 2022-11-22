35e498ac3dbba37831f1a7cca322f3575cf559b6
dev2820 <second28200@gmail.com>
2022-11-22 21:05:04 +0900

    feature/ guideview의 파일 불러오기 버튼 클릭시 모달이 나타나도록 수정

--- src/views/GuideView.vue
+++ src/views/GuideView.vue
@@ -5,7 +5,7 @@
                 >사용할 PDF를 불러오세요. 모르는 내용을 번역하고 중요한 내용을
                 노트에 필기해 논문을 집중해서 읽을 수 있습니다.</span
             >
-            <round-button class="load">
+            <round-button class="load" @click="modalStore.showModal">
                 {{ LOADER.VIEW.FILE_LOAD }}
             </round-button>
         </div>
@@ -14,6 +14,9 @@
 <script setup lang="ts">
 import RoundButton from '@/components/button/RoundButton.vue';
 import LOADER from '@/constants/LOADER';
+import { useModalStore } from '@/store/modal';
+
+const modalStore = useModalStore();
 </script>
 <style scoped lang="scss">
 @import '@/assets/scss/theme';
