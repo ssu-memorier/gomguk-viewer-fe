88ed3d548c2dc0c086b6ba11f809c5ff143b803f
dev2820 <second28200@gmail.com>
2022-11-22 11:27:51 +0900

    feature/ google 로그인 버튼 추가

---
+++ src/components/button/GoogleLoginButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="go(AUTH.BASE + AUTH.URL.LOGOUT)">
+        <img src="@/assets/images/png/google-login.png" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import AUTH from '@/constants/AUTH';
+
+function go(url: string) {
+    window.location.assign(url);
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+</style>
