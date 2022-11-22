e2db6f51fb2d51982559f0dc9233498da423b22d
dev2820 <second28200@gmail.com>
2022-11-22 11:00:56 +0900

    feature/ 카카오 로그인 버튼 생성

---
+++ src/components/button/KakaoLoginButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="go(AUTH.BASE + AUTH.URL.LOGOUT)">
+        {{ AUTH.PROVIDER.KAKAO }}
+    </button>
+</template>
+
+<script setup lang="ts">
+import AUTH from '@/constants/AUTH';
+
+function go(url: string) {
+    window.location.assign(url);
+}
+</script>
+
+<style lang="scss" scoped></style>
