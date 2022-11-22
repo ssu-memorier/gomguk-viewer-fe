a508cc606c9f29b5e89a1341ab142ff0fe9ed5e7
dev2820 <second28200@gmail.com>
2022-11-22 21:54:14 +0900

    feature/ column resizer도 props로 height를 조절하도록 수정

--- src/components/resizer/ColumnResizer.vue
+++ src/components/resizer/ColumnResizer.vue
@@ -3,6 +3,7 @@
         class="columnResizer"
         @mousemove="resize"
         @mouseup="resizeEnd"
+        :style="{ height: props.boxHeight + 'px' }"
         ref="$columnResizer"
     >
         <div class="top" :style="{ height: topHeight + 'px' }">
@@ -28,6 +29,10 @@ import { ref, onMounted, computed, defineProps } from 'vue';
 import RESIZER from '@/constants/RESIZER';
 
 const props = defineProps({
+    boxHeight: {
+        type: Number,
+        required: true,
+    },
     topPercent: {
         type: Number,
         validator(value: number) {
@@ -37,21 +42,16 @@ const props = defineProps({
 });
 const $resizer = ref();
 const $columnResizer = ref();
-const boxHeight = ref<number>(0);
 const boxOffset = ref<number>(0);
 const topPercent = ref<number>(props.topPercent ?? 0.5);
 const isResizing = ref<boolean>(false);
-const topHeight = computed(() => boxHeight.value * topPercent.value);
-const bottomHeight = computed(() => boxHeight.value - topHeight.value);
+const topHeight = computed(() => props.boxHeight * topPercent.value);
+const bottomHeight = computed(() => props.boxHeight - topHeight.value);
 
 onMounted(() => {
     const rect = $columnResizer.value.getBoundingClientRect();
-    boxHeight.value = rect.height;
-    boxOffset.value = rect.y;
-});
 
-window.addEventListener('resize', () => {
-    boxHeight.value = $columnResizer.value.getBoundingClientRect().height;
+    boxOffset.value = rect.y;
 });
 
 function resizeStart() {
@@ -63,7 +63,7 @@ function resizeEnd() {
 function resize(evt: MouseEvent) {
     if (!isResizing.value) return;
 
-    topPercent.value = (evt.clientY - boxOffset.value) / boxHeight.value;
+    topPercent.value = (evt.clientY - boxOffset.value) / props.boxHeight;
 }
 </script>
 <style scoped lang="scss">
@@ -80,6 +80,8 @@ function resize(evt: MouseEvent) {
 }
 div.resizer {
     position: absolute;
+    left: 0;
+    top: 0;
     z-index: 100;
     width: 100%;
     height: $RESIZER_LEN;
