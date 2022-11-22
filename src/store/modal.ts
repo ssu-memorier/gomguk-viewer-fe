a4c17a597952e4e8614726768c4292a2d55ad741
dev2820 <second28200@gmail.com>
2022-11-22 11:04:52 +0900

    feature/ 로그인 method 모달 토글 로직 추가

--- src/store/modal.ts
+++ src/store/modal.ts
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 export const useModalStore = defineStore('modal', () => {
     const isShow = ref<boolean>(false);
     const isShowProfileMenu = ref<boolean>(false);
+    const isShowLoginMethods = ref<boolean>(false);
 
     function showModal() {
         isShow.value = true;
@@ -16,11 +17,17 @@ export const useModalStore = defineStore('modal', () => {
         isShowProfileMenu.value = !isShowProfileMenu.value;
     }
 
+    function toggleLoginMethods() {
+        isShowLoginMethods.value = !isShowLoginMethods.value;
+    }
+
     return {
         isShow,
         showModal,
         hideModal,
         isShowProfileMenu,
         toggleProfileMenu,
+        isShowLoginMethods,
+        toggleLoginMethods,
     };
 });
