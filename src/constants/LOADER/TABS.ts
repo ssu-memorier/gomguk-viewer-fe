0a256ec474e14ac52dbefa24b78cbc2504330ad9
dev2820 <second28200@gmail.com>
2022-11-23 12:01:06 +0900

    chore/ 원격 저장소 -> 내 클라우드 명칭 변경

--- src/constants/LOADER/TABS.ts
+++ src/constants/LOADER/TABS.ts
@@ -1,5 +1,5 @@
 export default {
-    StorageLoad: { NAME: '원격 저장소', KEY: 'StorageLoad' },
+    StorageLoad: { NAME: '내 클라우드', KEY: 'StorageLoad' },
     FileUpload: { NAME: '업로드', KEY: 'FileUpload' },
     LocalFileLoad: { NAME: '로컬 파일 선택', KEY: 'LocalFileLoad' },
 } as const;
