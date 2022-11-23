b1491d7d777cbfa03d0cb3e3d53d710c12fa8942
dev2820 <second28200@gmail.com>
2022-11-23 11:42:15 +0900

    feature/ 로컬 파일 로드용 LocalFileLoadView 생성

---
+++ src/views/Loader/LocalFileLoadView.vue
@@ -0,0 +1,30 @@
+<template>
+    <pdf-load-button @load="loadPdfHandler" ref="$loadButton"></pdf-load-button>
+</template>
+<script lang="ts">
+export default {
+    name: 'LocalFileLoad',
+};
+</script>
+<script setup lang="ts">
+import PdfLoadButton from '@/components/button/PdfLoadButton.vue';
+import { usePdfStore } from '@/store/file/pdf';
+import { useEditorStore } from '@/store/file/editor';
+import { ref } from 'vue';
+import EDITOR from '@/constants/EDITOR';
+
+const $loadButton = ref();
+const pdfStore = usePdfStore();
+const editorStore = useEditorStore();
+
+async function loadPdfHandler(pdf: File) {
+    pdfStore.setPdfFile(pdf);
+    editorStore.fromJSON({
+        time: 1668084171608,
+        blocks: [EDITOR.DEFAULT],
+        version: '2.25.0',
+    });
+
+    return;
+}
+</script>
