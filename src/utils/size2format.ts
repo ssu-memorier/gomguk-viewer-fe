d40593f971dcf7fae7a355735be39bcf8e935153
dev2820 <second28200@gmail.com>
2022-11-23 16:36:17 +0900

    refactor/ 주석 추가

--- src/utils/size2format.ts
+++ src/utils/size2format.ts
@@ -1,5 +1,12 @@
 import FILE from '@/constants/FILE';
 
+/**
+ * size를 받아 MB,KB 형태로 변환해 반환하는 함수
+ * 소수점 1자리까지 같이 출력함 (ex. 2000B -> 1.9KB)
+ *
+ * @param size
+ * @returns [number]MB | [number]KB | [number]B
+ */
 export default function size2format(size: number) {
     if (size > FILE.SIZE.MB) {
         return (size / FILE.SIZE.MB).toFixed(1) + 'MB';
