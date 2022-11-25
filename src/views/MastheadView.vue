c1a5edf12e8ae81f83190d13ca888eee98f603fb
dev2820 <second28200@gmail.com>
2022-11-25 21:18:10 +0900

    feature/ header 중앙에 파일 제목 나오도록 수정

--- src/views/MastheadView.vue
+++ src/views/MastheadView.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="mastheadView">
         <site-logo></site-logo>
+        <span class="fileName">{{ pdfStore.fileName }}</span>
         <menu v-if="userStore.isLoggined">
             <round-button
                 class="save"
@@ -44,9 +45,11 @@ import { useUserStore } from '@/store/user';
 import HEADER from '@/constants/HEADER';
 import RoundButton from '@/components/button/RoundButton.vue';
 import ProfileButton from '@/components/button/ProfileButton.vue';
+import { usePdfStore } from '@/store/file/pdf';
 
 const modalStore = useModalStore();
 const fileStore = useFileStore();
+const pdfStore = usePdfStore();
 const userStore = useUserStore();
 
 function load() {
@@ -69,7 +72,9 @@ function toggleProfileMenu() {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-
+    span.fileName {
+        margin: auto 0;
+    }
     menu {
         padding: 0;
         margin: 0;
