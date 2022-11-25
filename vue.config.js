ce4e4a022fa36556d282635da4450d05a62804db
dev2820 <second28200@gmail.com>
2022-11-25 23:34:04 +0900

    chore/ PWA 관련 로직 제거

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: './',
+    chainWebpack: (config) => {
+        config.plugins.delete('pwa');
+        config.plugins.delete('workbox');
+    },
 });
