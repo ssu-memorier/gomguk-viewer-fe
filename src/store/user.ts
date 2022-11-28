52f20455af4ae7cd95c21119db27218c66ef0880
dev2820 <second28200@gmail.com>
2022-11-28 21:17:51 +0900

    feature/ 필요한 프로필 정보를 받도록 수정

--- src/store/user.ts
+++ src/store/user.ts
@@ -4,14 +4,23 @@ import { requestProfile } from '@/api/user';
 export const useUserStore = defineStore('user', () => {
     const isLoggined = ref<boolean>(false);
     const userName = ref<string>('');
+    const profileImage = ref<string>('');
+    const thumbnailProfileImage = ref<string>('');
 
     async function getProfile() {
         const response = await requestProfile();
         if (response.isSuccess) {
-            userName.value = response.payload.name;
+            const { name, profileImage, thumbnailProfileImage } =
+                response.payload;
+
+            userName.value = name;
+            profileImage.value = profileImage;
+            thumbnailProfileImage.value = thumbnailProfileImage;
             isLoggined.value = true;
         } else {
             userName.value = '';
+            profileImage.value = '';
+            thumbnailProfileImage.value = '';
             isLoggined.value = false;
         }
     }
@@ -19,6 +28,8 @@ export const useUserStore = defineStore('user', () => {
     return {
         isLoggined,
         userName,
+        profileImage,
+        thumbnailProfileImage,
         getProfile,
     };
 });
